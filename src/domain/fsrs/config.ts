a8b7a531a55bf816b30875c6b74b1f7673bca97a
Jedi-Master-Teban <earriaga@unal.edu.co>
2026-08-31 08:27:58 -0500

    Updates

--- src/domain/fsrs/config.ts
+++
@@ -1,3 +0,0 @@
-import { generatorParameters, type FSRSParameters } from 'ts-fsrs';
-
-export const FSRS_PARAMS: FSRSParameters = generatorParameters();
--- src/dominio-smoke.test.ts
+++
@@ -1,5 +0,0 @@
-describe('entorno de test', () => {
-  it('corre Jest correctamente', () => {
-    expect(1 + 1).toBe(2);
-  });
-});
